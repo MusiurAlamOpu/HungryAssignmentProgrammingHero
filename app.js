795f73a748c97590def974a3e82dc8bff279e007
Musiur Alam Opu <musiuralamo@gmail.com>
2021-02-08 15:55:54 +0600

    after completing the assignment

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ document.getElementById("searchButton").addEventListener("click", function(){
     console.log("Input Text is: " + inputText);
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + inputText)
     .then(response => response.json())
-    .then(data => displayMeal(data));
+    .then(data => displayMeal(data))
+    .catch(err => alert("Could not find the: " + inputText + "\ndue to:\n" + err.message));
 });
 
 function displayMeal(mealItems){
-    console.log("Length: " + mealItems.meals.length);
+    // console.log("Length: " + mealItems.meals.length);
     const mealDivs = document.getElementById("mealItemsList");
-    for(let i = 0; i < mealItems.meals.length; i++) {
-        const meal = mealItems.meals[i];
+    mealDivs.innerHTML = ``;
+    // for(let i = 0; i < mealItems.meals.length; i++) {
+    //     const meal = mealItems.meals[i];
+    //     const mealDiv = document.createElement("div");
+    //     mealDiv.className = "mealDivItems";
+    //     mealDiv.innerHTML = `
+    //         <h3>${meal.strMeal}</h3>
+    //     `;
+    //     console.log(meal.strMeal);
+    //     mealDivs.appendChild(mealDiv);
+    // }
+    mealItems.meals.forEach(meal => {
         const mealDiv = document.createElement("div");
         mealDiv.className = "mealDivItems";
         mealDiv.innerHTML = `
             <h3>${meal.strMeal}</h3>
+            <button class="mealDivElements>Ingradients</button>
         `;
         console.log(meal.strMeal);
         mealDivs.appendChild(mealDiv);
-    }
+        
+    });
 }
